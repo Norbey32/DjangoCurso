29309ea330e08147435a51d3ed3c7f2131f9bf69
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-09-09 18:20:59 -0500

    creando relaciones de muchos a muchos

---
+++ mi_sitio/my_first_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def my_view(request):
+    car_list = [
+        {"title": "Ford Ranger"},
+        {"title": "Chevrolet S10"},
+        {"title": "Toyota Hilux"},
+    ]
+
+    context = {
+        "car_list": car_list
+    }
+    return render(request, "my_first_app/car_list.html", context)
