8f8dfff26fa70f115522b54cc0122a840faece09
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-09-15 19:35:51 -0500

    Aprendiendo conceptos Django

--- mi_sitio/my_first_app/urls.py
+++ mi_sitio/my_first_app/urls.py
@@ -13,17 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.http import HttpResponse
 from django.urls import path
+from .views import CarListView, my_test_view
 
 
-def my_view(request, *args, **kwargs):
-    print(args)
-    print(kwargs)
-    return HttpResponse("")
+
 
 urlpatterns = [
-    path('listado/', my_view),
-    path('detalle/<int:id>/', my_view),
-    path('marcas/<str:brand>/', my_view),
+    #path('listado/', my_view),
+    path('listado/', CarListView.as_view()),
+    path('detalle/<int:id>/', my_test_view),
+    path('marcas/<str:brand>/', my_test_view),
 ]
--- mi_sitio/my_first_app/views.py
+++ mi_sitio/my_first_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
@@ -9,7 +11,25 @@ def my_view(request):
         {"title": "Toyota Hilux"},
     ]
 
-    context = {
+    context = { 
         "car_list": car_list
     }
     return render(request, "my_first_app/car_list.html", context)
+
+class CarListView(TemplateView):
+    template_name = "my_first_app/car_list.html"
+
+    def get_context_data(self):
+        car_list = [
+            {"title": "Ford Ranger"},
+            {"title": "Chevrolet S10"},
+            {"title": "Toyota Hilux"},
+        ]
+        return {
+            "car_list": car_list
+        }
+
+def my_test_view(request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    return HttpResponse("")
